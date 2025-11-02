1132d146cccd2a9587fb1f3d55dc3138884e60e3
yuraup <o721@naver.com>
2025-11-02 16:08:17 +0900

    test(lotto): 로또 클래스 관련 테스트

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,25 @@
-import Lotto from "../src/Lotto";
+import Lotto from '../src/Lotto';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
-  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test('범위(1~45)를 벗어나면 예외가 발생한다.', () => {
+    expect(() => new Lotto([0, 2, 3, 4, 5, 6])).toThrow('[ERROR]');
+    expect(() => new Lotto([1, 46, 3, 4, 5, 6])).toThrow('[ERROR]');
+  });
+
+  test('정상 생성 시 오름차순 정렬된 getNumbers를 반환한다.', () => {
+    const lotto = new Lotto([6, 1, 3, 2, 5, 4]);
+    expect(lotto.getNumbers()).toEqual([1, 2, 3, 4, 5, 6]);
+  });
 });
